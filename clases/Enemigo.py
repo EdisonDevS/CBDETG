eba81315020743956520664633a5b8cef747eb4b
Jhon Edison Giraldo <jhonedisongiraldo99@gmail.com>
2019-06-22 15:15:31 -0500

    nodificado el comportamiento de los enemigos para que corran cuando estén cerca del jugador

--- clases/Enemigo.py
+++ clases/Enemigo.py
@@ -5,27 +5,38 @@ class Enemigo(pygame.sprite.Sprite):
     
     def __init__(self, pos, mat_i):
         pygame.sprite.Sprite.__init__(self)
+        #tipo de enemigo
+        self.tipo_enemigo=0
+
         #control imagen
         self.accion = 0
-        self.animacion = 7
+        self.animacion = 1
         self.matriz = mat_i
-        self.limite = [8,8,7,9,9,9,9,6,6,6,6,4,4,4,4,6,5,4,6,3,3]
-        self.image = self.matriz[self.accion][self.animacion]
+        self.limite = [4,6,7,7,4,6,7,7,4,6,7,7,4,6,7,7,4,6,7,7,4,6,7,7,4,6,7,7,4,6,7,7]
+        self.image = self.matriz[self.accion][self.animacion+self.tipo_enemigo]
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
 
         #stats
-        self.velx = 0
-        self.vely = 0
+        self.velx = 1
+        self.vely = 1
         self.vida = 100
         self.cadencia = 30
         self.escudo = 0
         self.inmune = False
+        self.correr=False
 
 
     def update(self, player_position):
-        self.image=self.matriz[self.accion][self.animacion]
+        self.image=self.matriz[self.accion][self.animacion+self.tipo_enemigo]
+
+        if self.correr:
+            self.velx=2
+            self.vely=2
+        else:
+            self.velx=1
+            self.vely=1
 
         if self.accion < self.limite[self.animacion]-1:
             self.accion+=1
@@ -33,10 +44,27 @@ class Enemigo(pygame.sprite.Sprite):
             self.accion=0
 
         #seguir al jugador
-        if self.rect.x < player_position[0]:
-            self.rect.x +=1
-        else: self.rect.x -=1
-
-        if self.rect.y < player_position[1]:
-            self.rect.y +=1
-        else: self.rect.y -=1
+        if abs(player_position[0]-self.rect.x)<=1:
+            if self.correr:
+                self.animacion=3
+            else:
+                self.animacion=0
+        elif self.rect.x < player_position[0]:
+            self.rect.x += self.velx
+            if self.correr:
+                self.animacion=3
+            else:
+                self.animacion=1
+        elif self.rect.x > player_position[0]: 
+            self.rect.x -= self.velx
+            if self.correr:
+                self.animacion=7
+            else:
+                self.animacion=5
+        
+        if abs(player_position[1]-self.rect.y)<=1:
+            self.rect.y+=0
+        elif self.rect.y < player_position[1]:
+            self.rect.y += self.vely
+        else: 
+            self.rect.y -= self.vely
--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+from pygame.locals import *
+import sys
 from Util import *
 from clases.Jugador import *
 from clases.Bala import *
@@ -8,26 +10,33 @@ from clases.Enemigo import *
 if __name__ == '__main__':
     pygame.init()
     pantalla=pygame.display.set_mode([Util.ANCHO, Util.ALTO])
-    img= pygame.image.load('images/liche.png')
 
-    info=img.get_rect()
-    ancho_img=info[2]
-    alto_img=info[3]
 
-    m=Util.cut(img, 9, 21, 29, 33)
+    #configuracion del jugador
+    img_juagador= pygame.image.load('images/liche.png')
+
+    imagenesJugador=Util.cut(img_juagador, 9, 21, 29, 33)
+
+
+    #configuraion de los enemigos
+    img_enemigo= pygame.image.load('images/enemigos.png')
+
+    imagenesEnemigo=Util.cut(img_enemigo, 7, 32, 24, 24)
 
     #grupos
     jugadores=pygame.sprite.Group()
     balas=pygame.sprite.Group()
     enemigos=pygame.sprite.Group()
 
-    j=Jugador(Util.CENTRO,m)
+    j=Jugador(Util.CENTRO,imagenesJugador)
     jugadores.add(j)
     fin=False
     reloj=pygame.time.Clock()
 
     vuelo=0
     desplazamiento = [0,0]
+    fuente=pygame.font.Font(None, 20)
+    puntaje=0
 
     while not fin:
 
@@ -37,7 +46,8 @@ if __name__ == '__main__':
 
         if posibilidad_enemigo in [100,50,0]:
             coordenadas=[random.randint(0,Util.ANCHO), random.randint(0,Util.ALTO)]
-            e=Enemigo(coordenadas, m)
+            e=Enemigo(coordenadas, imagenesEnemigo)
+            e.tipo_enemigo=random.randint(0,3)*8
             enemigos.add(e)
 
         for event in pygame.event.get():
@@ -68,7 +78,7 @@ if __name__ == '__main__':
                     if j.animacion != 0:
                             j.animacion = 1
                 if event.button==1:
-                    b=Bala([j.rect.x, j.rect.y], pygame.mouse.get_pos(), m)
+                    b=Bala([j.rect.x, j.rect.y], pygame.mouse.get_pos(), imagenesJugador)
                     balas.add(b)
 
         #elimina la bala de memoria cuando sale de la pantalla
@@ -76,6 +86,12 @@ if __name__ == '__main__':
             if (b.rect.x > Util.ANCHO) or (b.rect.x < 0) or (b.rect.y > Util.ALTO) or (b.rect.y < 0):
                 balas.remove(b)
 
+        #el enemigo corre cuando está a determinada distancia del jugador
+        for e in enemigos:
+            if pygame.sprite.collide_circle_ratio(2.0)(j,e):
+                e.correr=True
+            else:
+                e.correr=False
 
         #COLISIONES
         for b in balas:
@@ -90,15 +106,24 @@ if __name__ == '__main__':
         desplazamiento = [(j.rect.x - 100*desplazamiento[0]+j.rect.width/2),(j.rect.y - 100*desplazamiento[1]+j.rect.height/2)]
         '''
         player_position=[]
+
         
         for j in jugadores:
             player_position=j.getPosition()
+
 
         balas.update()
         jugadores.update()
         enemigos.update(player_position)
         pantalla.fill(Util.BLANCO)
-        
+
+
+        #se muestran los puntajes
+        """
+        texto="Puntaje: "+str(puntaje)
+        textoPuntaje=fuente.render(texto, 1, Util.NEGRO)
+        pantalla.blit(textoPuntaje,[100,100])
+        """
         '''
         pygame.draw.line(pantalla, Util.ROJO, [int(j.rect.x+j.rect.width/2), int(j.rect.y+j.rect.height/2)], desplazamiento, 1)
         pygame.draw.circle(pantalla, Util.NEGRO, [int(j.rect.x+j.rect.width/2), int(j.rect.y+j.rect.height/2)], 100, 1)
