7a9d11b51778d8f7c0ce0cd6aa9dddd305ed3a3b
Jhon Edison Giraldo <jhonedisongiraldo99@gmail.com>
2019-06-25 21:45:26 -0500

    los botiquines ahora hacen que se recupere 40 de vida, ahora es mas jugable esa vaina :v

--- game.py
+++ game.py
@@ -168,6 +168,16 @@ if __name__ == '__main__':
                     jugador.vida-=10
                     balas_enemigas.remove(b)
 
+
+        #COLISIONES JUGADOR - BOTIQUIN
+        for b in botiquines:
+            ls_col = pygame.sprite.spritecollide(b, jugadores, False)
+            for jugador in ls_col:
+                if(jugador.vida > 0):
+                    jugador.vida+=40
+                    botiquines.remove(b)
+
+
         '''
         inicio = [j.rect.x,j.rect.y]
         end = pygame.mouse.get_pos()
